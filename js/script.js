50d28e375bd14798b23ed559a743d04c1c8c39c8
Sergjio97 <sergiocizmja@gmail.com>
2021-11-24 17:36:35 +0100

    timer finito

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
 
-let randomNumberBox = document.getElementById('randomNumber_box');
-
 function randomNumber() {
     for (let i = 1; i <= 5; i++) {
 
         const randomNumber = Math.floor(Math.random() * 9) + 1;
-        let randomNumberBox = document.getElementById('randomNumber_box').innerHTML += randomNumber;
+        document.getElementById('randomNumber_box').innerHTML += randomNumber;
 
     };
 }
 
 randomNumber();
 
-const clock = setInterval(timer, 1000);
+
+
+let timeout3s = setTimeout(timeout, 3000);
+
+function timeout (){
+    const clock = setInterval(timer, 1000);
+}
 
 let second = 30;
 
 function timer (){
+
+    document.getElementById('randomNumber_box').style.display = "none"; 
     document.getElementById('timer_box').innerHTML = second-- + 's';
     if (second == -1){
         clearInterval(clock);
