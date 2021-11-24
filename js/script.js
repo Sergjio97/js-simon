8419bb8b98a5b8801c65a3b490666abdec916d88
Sergjio97 <sergiocizmja@gmail.com>
2021-11-24 17:46:06 +0100

    prompt inserisci numeri

--- js/script.js
+++ js/script.js
@@ -1,22 +1,19 @@
+const numeriRandom = [];
 
 function randomNumber() {
     for (let i = 1; i <= 5; i++) {
 
         const randomNumber = Math.floor(Math.random() * 9) + 1;
         document.getElementById('randomNumber_box').innerHTML += randomNumber;
+        numeriRandom.push(randomNumber);
+        console.log(numeriRandom);
 
     };
 }
 
 randomNumber();
 
-
-
-let timeout3s = setTimeout(timeout, 3000);
-
-function timeout (){
-    const clock = setInterval(timer, 1000);
-}
+const clock = setInterval(timer, 1000);
 
 let second = 30;
 
@@ -27,6 +24,15 @@ function timer (){
     if (second == -1){
         clearInterval(clock);
     }   
+}
+
+for (let i = 0; i < 5; i++){
+    let user = parseInt(prompt('Inserisci i numeri che hai visualizzato'))
+    if (user == numeriRandom){
+        alert('hai vinto')
+    } else {
+        alert('hai perso')
+    }
 }
 
 
